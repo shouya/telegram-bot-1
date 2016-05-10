8b60006f4e447fbbc711ae5840b70927a314c0f7
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2016-05-10 14:41:54 +0000

    rebuild pages at 0f27368

--- implementors/collections/range/trait.RangeArgument.js
+++ implementors/collections/range/trait.RangeArgument.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bitflags'] = [];implementors['regex_syntax'] = [];implementors['hyper'] = [];implementors['multipart'] = [];
+implementors['regex_syntax'] = [];implementors['hyper'] = [];implementors['multipart'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/std/sys_common/thread_info/trait.NewThread.js
+++ implementors/std/sys_common/thread_info/trait.NewThread.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bitflags'] = [];implementors['openssl'] = [];
+implementors['openssl'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
