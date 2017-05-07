e1cb51fc7fcb8ce266ecca7f9d7488fbf869c569
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-05-07 08:08:31 +0000

    rebuild pages at 3fec699

--- telegram_bot_raw/requests/_base/sidebar-items.js
+++ telegram_bot_raw/requests/_base/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["ParseMode","Strongly typed ParseMode. See documentation for details."]],"mod":[["reply_markup",""]],"trait":[["Request","Request corresponds to the specific Telegram API method."]]});
+initSidebarItems({"enum":[["ParseMode","Strongly typed ParseMode. See documentation for details."]],"mod":[["reply_markup",""]],"trait":[["Request","Request corresponds to the specific Telegram API method."],["ToReplyRequest",""],["ToRequest",""]]});
