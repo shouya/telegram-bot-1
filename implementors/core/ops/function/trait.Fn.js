c95b2e48dd5618ee05151d2c2d50e2222825bdb2
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-09-24 18:25:06 +0000

    rebuild pages at 7f723cd

---
+++ implementors/core/ops/function/trait.Fn.js
@@ -0,0 +1,18 @@
+(function() {var implementors = {};
+implementors["antidote"] = [];
+implementors["bytes"] = [];
+implementors["error_chain"] = [];
+implementors["libc"] = [];
+implementors["mio"] = [];
+implementors["serde"] = [];
+implementors["syn"] = [];
+implementors["telegram_bot"] = [];
+implementors["tokio_core"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
